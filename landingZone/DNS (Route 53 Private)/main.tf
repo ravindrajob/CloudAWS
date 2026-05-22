489681cb378bb9859597a91321f29ca2cb7b388c
ravin <ravin@ravin-cloud.fr>
2026-05-22 21:36:58 +0200

    feat: maillage DNS Zero Trust et entrees de simulation Lab (v1.2)

--- landingZone/DNS (Route 53 Private)/main.tf
+++ landingZone/DNS (Route 53 Private)/main.tf
@@ -1,14 +1,17 @@
 ################################################################
-# Titre: AWS Route 53 Private DNS
-# Description : Gestion des zones DNS privées pour ravindra-job.com
+# Titre: DNS (Route 53 Private)
+# Description : Maillage Zéro Trust et entrées de simulation Lab
 # Auteur: Ravindra JOB
 # Source: https://github.com/ravindrajob/
-# Update: 15/10/2025 [v1.0 | RJ]
+# Update: 22/05/2026 [v1.2 | RJ]
 ################################################################
 
-resource "aws_route53_zone" "private" {
+# 1. Zone DNS Privée (CAF: Hybrid Connectivity DNS)
+resource "aws_route53_zone" "private_zone" {
   name = "ravindra-job.com"
 
+  # Liaison physique au VPC (VPC Link)
+  # Indispensable pour la résolution sémantique interne
   vpc {
     vpc_id = var.vpc_id
   }
@@ -19,11 +22,28 @@ resource "aws_route53_zone" "private" {
   }
 }
 
-# Exemple d'enregistrement pour le Lab
-resource "aws_route53_record" "n8n" {
-  zone_id = aws_route53_zone.private.zone_id
-  name    = "n8n.ravindra-job.com"
+# 2. Enregistrements DNS de démonstration (Simulation Lab)
+
+resource "aws_route53_record" "api_endpoint" {
+  zone_id = aws_route53_zone.private_zone.zone_id
+  name    = "api.ravindra-job.com"
   type    = "A"
   ttl     = "300"
-  records = [var.n8n_private_ip]
+  records = ["10.100.0.100"] # IP fictive de l'ALB interne
+}
+
+resource "aws_route53_record" "eks_internal" {
+  zone_id = aws_route53_zone.private_zone.zone_id
+  name    = "k8s-master.ravindra-job.com"
+  type    = "A"
+  ttl     = "300"
+  records = ["10.1.1.254"] # IP fictive Endpoint EKS
+}
+
+resource "aws_route53_record" "bedrock_privatelink" {
+  zone_id = aws_route53_zone.private_zone.zone_id
+  name    = "bedrock-ai.ravindra-job.com"
+  type    = "CNAME"
+  ttl     = "300"
+  records = ["vpce-0123456789abcdef.bedrock-runtime.eu-west-3.vpce.amazonaws.com"] # Redirection PrivateLink
 }
